aa805d9f56eb227a1d41231fb522975b0b0ddeb7
Congcong Cai (EC-EE-JD-4) <congcong.cai@bmw.com>
2025-03-28 10:42:10 +0800

    feat: change pass order to improve optimization result (#31)

--- passes/Runner.cpp
+++ passes/Runner.cpp
@@ -51,12 +51,12 @@ static passes::Output runImpl(std::unique_ptr<wasm::Module> &m) {
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::createExtractMostFrequentlyUsedGlobalsPass()));
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::createInlineSetterFunctionPass()));
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::as_gc::createRemoveDuplicateStoreLocalInGCPass()));
-    passRunner.add("optimize-instructions");
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::as_gc::createCleanLeafFunctionGC()));
+    passRunner.add("vacuum"); // add vacuum to remove dead code
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::as_gc::createCleanDirectLocalUsesGC()));
-    passRunner.add("optimize-instructions");
+    passRunner.add("vacuum");
     passRunner.add(std::unique_ptr<wasm::Pass>(passes::as_gc::createRemoveDuplicateStoreLocalInGCPass()));
-    passRunner.add("optimize-instructions");
+    passRunner.add("vacuum");
 
     passRunner.run();
     ensureValidate(*m);
