560fc1ac411eccbdca9df3ef9204f8c07a8cdf0d
Congcong Cai <congcongcai0907@163.com>
2025-10-30 09:30:36 +0800

    wip

---
+++ third_party/warp_runner/WarpRunner.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2025 wasm-ecosystem
+// SPDX-License-Identifier: Apache-2.0
+
+#include "WarpRunner.hpp"
+
+#include "src/utils/StackTop.hpp"
+
+WarpRunner::WarpRunner(void *const ctx)
+    : logger(), stackTop(static_cast<uint8_t const *>(vb::getStackTop())), m{logger} {
+  m.setStacktraceRecordCount(32U);
+  m.setContext(ctx);
+}
