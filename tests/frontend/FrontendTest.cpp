4f8c83c78935ac0018b03f6098f837a325204143
Changqing Jing <changqing.jing@bmw.com>
2025-11-05 17:13:15 +0800

    Fix

--- tests/frontend/FrontendTest.cpp
+++ tests/frontend/FrontendTest.cpp
@@ -273,7 +273,7 @@ void collectTestFilesImpl(std::vector<std::filesystem::path> &testFiles, std::fi
 
 std::filesystem::path getTestFolder() {
   std::filesystem::path const folder = std::filesystem::path{__FILE__}.parent_path() / "compiler";
-  return folder;
+  return std::filesystem::absolute(folder);
 }
 
 std::vector<std::filesystem::path> collectTestFiles(std::filesystem::path const &folder) {
@@ -293,6 +293,7 @@ void frontendTestMain(int argc, const char *argv[]) {
   cli::init(cli::Category::OnlyForTest, program, argc, argv);
 
   std::filesystem::path const testFolder = getTestFolder();
+  std::cout << "Test folder: " << testFolder.string() << std::endl;
   std::filesystem::current_path(testFolder);
   std::vector<std::filesystem::path> const testFiles = collectTestFiles(testFolder);
 
