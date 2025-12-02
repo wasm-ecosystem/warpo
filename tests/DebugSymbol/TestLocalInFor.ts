e36b3e6d092530b2abd8e6eca7117adee05b1beb
Changqing Jing <changqing.jing@bmw.com>
2025-12-02 16:36:26 +0800

    Fix

--- tests/DebugSymbol/TestLocalInFor.ts
+++ tests/DebugSymbol/TestLocalInFor.ts
@@ -1,14 +1,14 @@
 export let a = 10;
-export function foo():i32{
-    for(let iii = 0;iii<10;iii++){
-        if(iii % 2 == 0){
-            let bbb = iii * 2;
-            a += bbb;
-        }else{
-            let ccc = iii + 3;
-            a += ccc;
-        }
+export function foo(): i32 {
+  for (let iii = 0; iii < 10; iii++) {
+    if (iii % 2 == 0) {
+      let bbb = iii * 2;
+      a += bbb;
+    } else {
+      let ccc = iii + 3;
+      a += ccc;
     }
-    return a;
+  }
+  return a;
 }
 foo();
