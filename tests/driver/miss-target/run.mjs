27e027704ad580683071ccd97fc4d28a4977bf1c
Cai Congcong <congcong.cai@bmw.com>
2026-03-04 15:38:53 +0800

    fix

--- tests/driver/miss-target/run.mjs
+++ tests/driver/miss-target/run.mjs
@@ -1,20 +1,26 @@
+import expect from "expect";
 import { env } from "node:process";
 import { describe, it } from "node:test";
-import expect from "expect";
 import { main as warpoMain } from "../../../dist/warpo.js";
 
 const __dirname = import.meta.dirname;
 const projectRoot = __dirname;
 
-describe("driver: compilation-failed", () => {
+describe("driver: missing-target", () => {
   it("reports missing target", async () => {
+    let stdout = [];
     const code = await warpoMain({
       argv: ["build", "--target", "miss"],
       env: { ...env, FORCE_COLOR: "0" },
       cwd: projectRoot,
+      onStdout: (data) => {
+        stdout.push(data);
+      },
     });
 
     expect(code).not.toBe(0);
-    // expect(stdout).toContain("ERROR: Target 'miss' not found in asconfig.json. Available targets: debug, release.");
+    expect(stdout.join("")).toContain(
+      "ERROR: Target 'miss' not found in asconfig.json. Available targets: debug, release."
+    );
   });
 });
