f7d28bea3e35e2e67b57afb15f53178cf7732e62
Cai Congcong <congcong.cai@bmw.com>
2026-03-13 16:16:41 +0800

    rename

--- common/MemoryOption.cpp
+++ common/MemoryOption.cpp
@@ -12,39 +12,57 @@ namespace warpo::common {
 
 namespace {
 
-uint64_t parseMaximumMemoryAsBytes(std::string const &rawValue) {
-  auto const begin = rawValue.find_first_not_of(" \t\n\r");
-  if (begin == std::string::npos)
-    throw std::runtime_error{"maximumMemory cannot be empty"};
-
-  auto const end = rawValue.find_last_not_of(" \t\n\r");
-  std::string const value = rawValue.substr(begin, end - begin + 1);
-
-  size_t numberEnd = 0;
-  while (numberEnd < value.size() && std::isdigit(static_cast<unsigned char>(value[numberEnd])) != 0)
-    ++numberEnd;
-  if (numberEnd == 0)
-    throw std::runtime_error{fmt::format("Invalid maximumMemory value: {}", rawValue)};
-
+struct ParsedMaximumMemoryValue {
   uint64_t amount = 0;
+  std::string unit;
+};
+
+ParsedMaximumMemoryValue parseMaximumMemoryValue(std::string const &value) {
+  size_t begin = 0;
+  while (begin < value.size() && std::isspace(static_cast<unsigned char>(value[begin])) != 0)
+    ++begin;
+
+  size_t end = value.size();
+  while (end > begin && std::isspace(static_cast<unsigned char>(value[end - 1])) != 0)
+    --end;
+
+  size_t numberEnd = begin;
+  while (numberEnd < end && std::isdigit(static_cast<unsigned char>(value[numberEnd])) != 0)
+    ++numberEnd;
+  if (numberEnd == begin)
+    throw std::runtime_error{fmt::format("Invalid maximumMemory value: {}", value)};
+
+  ParsedMaximumMemoryValue parsed;
   try {
-    amount = std::stoull(value.substr(0, numberEnd));
+    parsed.amount = std::stoull(value.substr(begin, numberEnd - begin));
   } catch (std::exception const &) {
-    throw std::runtime_error{fmt::format("Invalid maximumMemory value: {}", rawValue)};
+    throw std::runtime_error{fmt::format("Invalid maximumMemory value: {}", value)};
   }
 
-  std::string unit;
-  unit.reserve(value.size() - numberEnd);
-  for (size_t i = numberEnd; i < value.size(); ++i) {
+  size_t unitStart = numberEnd;
+  while (unitStart < end && std::isspace(static_cast<unsigned char>(value[unitStart])) != 0)
+    ++unitStart;
+
+  parsed.unit.reserve(value.size() - unitStart);
+  for (size_t i = unitStart; i < end; ++i) {
     char const c = value[i];
     if (std::isspace(static_cast<unsigned char>(c)) != 0)
       continue;
-    unit.push_back(static_cast<char>(std::tolower(static_cast<unsigned char>(c))));
+
+    parsed.unit.push_back(static_cast<char>(std::tolower(static_cast<unsigned char>(c))));
   }
 
-  auto multiply = [amount, rawValue](uint64_t factor) -> uint64_t {
+  return parsed;
+}
+
+uint64_t parseMaximumMemoryAsBytes(std::string const &value) {
+  ParsedMaximumMemoryValue const parsed = parseMaximumMemoryValue(value);
+  uint64_t const amount = parsed.amount;
+  std::string const &unit = parsed.unit;
+
+  auto multiply = [amount, value](uint64_t factor) -> uint64_t {
     if (amount > std::numeric_limits<uint64_t>::max() / factor)
-      throw std::runtime_error{fmt::format("maximumMemory is too large: {}", rawValue)};
+      throw std::runtime_error{fmt::format("maximumMemory is too large: {}", value)};
     return amount * factor;
   };
 
@@ -59,14 +77,12 @@ uint64_t parseMaximumMemoryAsBytes(std::string const &rawValue) {
   if (unit == "p" || unit == "page" || unit == "pages")
     return multiply(65536ULL);
 
-  throw std::runtime_error{fmt::format("Unsupported maximumMemory unit: {}", rawValue)};
+  throw std::runtime_error{fmt::format("Unsupported maximumMemory unit: {}", value)};
 }
 
 } // namespace
 
-MaximumMemory MaximumMemory::parse(std::string const &rawValue) {
-  return MaximumMemory{parseMaximumMemoryAsBytes(rawValue)};
-}
+MaximumMemory MaximumMemory::parse(std::string const &value) { return MaximumMemory{parseMaximumMemoryAsBytes(value)}; }
 
 std::optional<uint32_t> MaximumMemory::toMaximumMemoryOption() const {
   constexpr uint64_t wasmPageSize = 65536ULL;
@@ -98,6 +114,28 @@ std::optional<uint32_t> MaximumMemory::toLowMemoryLimitOption() const {
 #include <gtest/gtest.h>
 
 namespace warpo::common::ut {
+
+TEST(MaximumMemoryTest, ParseMaximumMemoryValueAmountAndUnit) {
+  ParsedMaximumMemoryValue const parsed = parseMaximumMemoryValue(" 64kb ");
+  EXPECT_EQ(parsed.amount, 64ULL);
+  EXPECT_EQ(parsed.unit, "kb");
+}
+
+TEST(MaximumMemoryTest, ParseMaximumMemoryValueNormalizeUnit) {
+  ParsedMaximumMemoryValue const parsed = parseMaximumMemoryValue("32 KiB");
+  EXPECT_EQ(parsed.amount, 32ULL);
+  EXPECT_EQ(parsed.unit, "kib");
+}
+
+TEST(MaximumMemoryTest, ParseMaximumMemoryValueInvalidThrows) {
+  EXPECT_THROW((void)parseMaximumMemoryValue("abc"), std::runtime_error);
+}
+
+TEST(MaximumMemoryTest, ParseMaximumMemoryValueInternalSpaceNormalize) {
+  ParsedMaximumMemoryValue const parsed = parseMaximumMemoryValue("32 Ki B");
+  EXPECT_EQ(parsed.amount, 32ULL);
+  EXPECT_EQ(parsed.unit, "kib");
+}
 
 TEST(MaximumMemoryTest, ParseAsBytesAndLowMemoryLimit) {
   MaximumMemory const parsed = MaximumMemory::parse("65535");
