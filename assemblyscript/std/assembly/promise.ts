91412edf9ed2195d3b7cb51897bb8d13618c1b25
Changqing Jing <changqing.jing@bmw.com>
2026-04-20 17:24:21 +0800

    WIP

---
+++ assemblyscript/std/assembly/promise.ts
@@ -0,0 +1,149 @@
+enum _AsPromiseState {
+  Pending,
+  Fulfilled,
+  Rejected,
+}
+
+export class _AsPromiseBase {
+  private onThen: ((value: Object | null) => void) | null = null;
+  private onCatch: ((reason: Object | null) => void) | null = null;
+  private state: _AsPromiseState = _AsPromiseState.Pending;
+  private fulfilledValue: Object | null = null;
+  private rejectedReason: Object | null = null;
+  constructor(executor: (resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => void) {
+    let handleResolve = (value: Object | null): void => {
+      this.state = _AsPromiseState.Fulfilled;
+      this.fulfilledValue = value;
+      if (this.onThen) {
+        this.onThen(value);
+      }
+    };
+
+    let handleReject = (reason: Object | null): void => {
+      this.state = _AsPromiseState.Rejected;
+      this.rejectedReason = reason;
+      if (this.onCatch) {
+        this.onCatch(reason);
+      }
+    };
+
+    executor(handleResolve, handleReject);
+  }
+
+  thenBase(
+    onFulfilled: ((value: Object | null) => Object | null) | null,
+    onRejected: ((reason: Object | null) => Object | null) | null = null
+  ): _AsPromiseBase {
+    return new _AsPromiseBase((resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => {
+      this.onThen = (value: Object | null) => {
+        if (onFulfilled) {
+          const result = onFulfilled(value);
+          if (result instanceof _AsPromiseBase) {
+            (result as _AsPromiseBase).thenBase(
+              (v: Object | null): Object | null => {
+                resolve(v);
+                return null;
+              },
+              (reason: Object | null): Object | null => {
+                reject(reason);
+                return null;
+              }
+            );
+          } else {
+            //returns a value, resolve it
+            resolve(result);
+          }
+        } else {
+          resolve(value);
+        }
+      };
+
+      this.onCatch = (reason: Object | null) => {
+        if (onRejected) {
+          const result = onRejected(reason);
+          if (result instanceof _AsPromiseBase) {
+            (result as _AsPromiseBase).thenBase(
+              (v: Object | null): Object | null => {
+                resolve(v);
+                return null;
+              },
+              (newReason: Object | null): Object | null => {
+                reject(newReason);
+                return null;
+              }
+            );
+          } else {
+            resolve(result);
+          }
+        } else {
+          reject(reason);
+        }
+      };
+
+      if (this.state === _AsPromiseState.Fulfilled) {
+        this.onThen(this.fulfilledValue);
+      } else if (this.state === _AsPromiseState.Rejected) {
+        this.onCatch(this.rejectedReason);
+      }
+    });
+  }
+
+  catch(onRejected: (reason: Object | null) => Object | null): _AsPromiseBase {
+    return this.thenBase(null, onRejected);
+  }
+}
+
+export class Promise<T> extends _AsPromiseBase {
+  then<U>(
+    onFulfilled: (value: T | null) => Object | null,
+    onRejected: ((reason: Object | null) => Object | null) | null = null
+  ): Promise<U> {
+    let onFulfilledWrapper = (value: Object | null): Object | null => {
+      return onFulfilled(value as T | null);
+    };
+    let p: _AsPromiseBase = super.thenBase(onFulfilledWrapper, onRejected);
+    return new Promise<U>((resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => {
+      p.thenBase(
+        (value: Object | null): Object | null => {
+          resolve(value);
+          return null;
+        },
+        (reason: Object | null): Object | null => {
+          reject(reason);
+          return null;
+        }
+      );
+    });
+  }
+
+  static all(promiseArr: _AsPromiseBase[]): Promise<(Object | null)[]> {
+    return new Promise<(Object | null)[]>(
+      (resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => {
+        let counter = 0;
+        let values: (Object | null)[] = new Array<Object | null>();
+        for (let i = 0; i < promiseArr.length; i++) {
+          promiseArr[i].thenBase((value: Object | null): Object | null => {
+            values.push(value);
+            counter++;
+            if (counter == promiseArr.length) {
+              resolve(values);
+            }
+            return null;
+          });
+        }
+      }
+    );
+  }
+
+  static resolve<U>(value: U | null): Promise<U> {
+    return new Promise<U>((resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => {
+      resolve(value);
+    });
+  }
+
+  static reject<U>(reason: Object | null): Promise<U> {
+    return new Promise<U>((resolve: (value: Object | null) => void, reject: (reason: Object | null) => void) => {
+      reject(reason);
+    });
+  }
+}
