e7b91c7cf7bb239a25dd95838fb8eeb01a8be6d1
Changqing Jing <changqing.jing@bmw.com>
2026-07-02 12:07:30 +0800

    Add bench

--- tests/bootstrap/release.sh
+++ tests/bootstrap/release.sh
@@ -22,18 +22,20 @@ echo "second build"
   --asc-wasm tests/bootstrap/tmp/release/assemblyscript.warpo-test.wasm \
   --use ASC_GC_TESTING=1 ASC_GC_IDLEFACTOR=0 ASC_GC_GRANULARITY=64 ASC_GC_SWEEPFACTOR=1000 \
   -o tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wasm
-  
-echo "third build"
-./build/warpo/warpo_asc assemblyscript/src/glue/wasm/index.ts assemblyscript/src/index-wasm.ts \
-  --stats \
-  --optimizeLevel 3 --shrinkLevel 2 \
-  --disable-feature nontrapping-f2i \
-  --initialMemory 768 \
-  --exportStart _initialize --exportRuntime --exportTable \
-  --asc-wasm tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wasm \
-  --use ASC_GC_TESTING=1 ASC_GC_IDLEFACTOR=0 ASC_GC_GRANULARITY=64 ASC_GC_SWEEPFACTOR=1000 \
-  -o tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wasm
-  
-git --no-pager diff --no-index tests/bootstrap/tmp/release/assemblyscript.warpo-test.wat tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wat
+
+if [ "${BOOTSTRAP_RELEASE_SKIP_THIRD:-0}" != "1" ]; then
+  echo "third build"
+  ./build/warpo/warpo_asc assemblyscript/src/glue/wasm/index.ts assemblyscript/src/index-wasm.ts \
+    --stats \
+    --optimizeLevel 3 --shrinkLevel 2 \
+    --disable-feature nontrapping-f2i \
+    --initialMemory 768 \
+    --exportStart _initialize --exportRuntime --exportTable \
+    --asc-wasm tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wasm \
+    --use ASC_GC_TESTING=1 ASC_GC_IDLEFACTOR=0 ASC_GC_GRANULARITY=64 ASC_GC_SWEEPFACTOR=1000 \
+    -o tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wasm
+
+  git --no-pager diff --no-index tests/bootstrap/tmp/release/assemblyscript.warpo-test.wat tests/bootstrap/tmp/release/assemblyscript.warpo-test-bootstrap.wat
+fi
 
 echo "success"
