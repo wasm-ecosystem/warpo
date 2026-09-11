4732a60d2d57394295042e7567e22f0e9e56b41e
Changqing Jing <changqing.jing@bmw.com>
2026-09-11 12:55:03 +0800

    Fix

--- tests/assemblyscript/ast/closureScanner.test.ts
+++ tests/assemblyscript/ast/closureScanner.test.ts
@@ -441,7 +441,7 @@ describe("closureScanner", () => {
         expect(info.nestedLevel).equal(2);
       } else if (name === "<for>") {
         expect(info.closureVariables.size).equal(2);
-        expect(info.forInitClosureVariables.size).equal(1);
+        expect(info.forInitClosureVariables.size).equal(2);
         expect(info.nestedLevel).equal(1);
       } else if (name === "outer") {
         expect(info.closureVariables.size).equal(0);
